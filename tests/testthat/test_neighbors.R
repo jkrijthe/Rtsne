6570a54f7de9454bace0dce0b60b513497f4bfd1
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-07 19:05:45 +0100

    Added extra NN tests with normalized inputs.

--- tests/testthat/test_neighbors.R
+++ tests/testthat/test_neighbors.R
@@ -7,23 +7,53 @@ set.seed(101)
 # Hence, I have to simulate a case where no ties are possible.
 simdata <- matrix(rnorm(234*5), nrow=234)
 
-test_that("Rtsne works correctly with nearest-neighbor input", {
-    D <- as.matrix(dist(simdata))
-    all.indices <- matrix(0L, nrow(simdata), 90) # 3 * perplexity
-    all.distances <- matrix(0, nrow(simdata), 90)
-    for (i in seq_len(nrow(simdata))) {
+NNFUN <- function(D, K) 
+# A quick reference function for computing NNs, to avoid depending on other packages.
+{
+    all.indices <- matrix(0L, nrow(D), K)
+    all.distances <- matrix(0, nrow(D), K)
+    for (i in seq_len(nrow(D))) {
         current <- D[i,]
         by.dist <- setdiff(order(current), i)
         all.indices[i,] <- head(by.dist, ncol(all.indices))
         all.distances[i,] <- current[all.indices[i,]]
     }
+    list(index=all.indices, distance=all.distances)
+}
+
+test_that("Rtsne with nearest-neighbor input compares to distance matrix input", {
+    D <- as.matrix(dist(simdata))
+    out <- NNFUN(D, 90) # 3 * perplexity
+    all.indices <- out$index
+    all.distances <- out$distance
 
     # The vptree involves a few R::runif calls, which alters the seed in the precomputed distance case.
     # This results in different random initialization of Y.
     # Thus, we need to supply a pre-computed Y as well.
     Y_in <- matrix(runif(nrow(simdata)*2), ncol=2)
-    out <- Rtsne_neighbors(all.indices, all.distances, Y_init=Y_in, perplexity=30)
-    blah <- Rtsne(D, is_distance=TRUE, Y_init=Y_in, perplexity=30)
+    out <- Rtsne_neighbors(all.indices, all.distances, Y_init=Y_in, perplexity=30, max_iter=1)
+    blah <- Rtsne(D, is_distance=TRUE, Y_init=Y_in, perplexity=30, max_iter=1)
+    expect_equal(out$Y, blah$Y)
+
+    # Trying again with a different number of neighbors.
+    Y_in <- matrix(runif(nrow(simdata)*2), ncol=2)
+    out <- Rtsne_neighbors(all.indices[,1:30], all.distances[,1:30], Y_init=Y_in, perplexity=10)
+    blah <- Rtsne(D, is_distance=TRUE, Y_init=Y_in, perplexity=10)
+    expect_equal(out$Y, blah$Y)
+})
+
+test_that("Rtsne with nearest-neighbor input behaves upon normalization", {
+    D <- as.matrix(dist(normalize_input(simdata)))
+    out <- NNFUN(D, 90) # 3 * perplexity
+    all.indices <- out$index
+    all.distances <- out$distance
+
+    # The vptree involves a few R::runif calls, which alters the seed in the precomputed distance case.
+    # This results in different random initialization of Y.
+    # Thus, we need to supply a pre-computed Y as well.
+    Y_in <- matrix(runif(nrow(simdata)*2), ncol=2)
+    out <- Rtsne_neighbors(all.indices, all.distances, Y_init=Y_in, perplexity=30, max_iter=1)
+    blah <- Rtsne(D, is_distance=TRUE, Y_init=Y_in, perplexity=30, max_iter=1)
     expect_equal(out$Y, blah$Y)
 
     # Trying again with a different number of neighbors.
