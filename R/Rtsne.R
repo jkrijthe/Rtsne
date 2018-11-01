ebed20f612712987fd160386132c17289169b4d8
Jesse Krijthe <jkrijthe@gmail.com>
2018-11-01 15:22:23 +0100

    Adds more examples to documentation

--- R/Rtsne.R
+++ R/Rtsne.R
@@ -76,19 +76,37 @@
 #' @examples
 #' iris_unique <- unique(iris) # Remove duplicates
 #' iris_matrix <- as.matrix(iris_unique[,1:4])
-#' set.seed(42) # Set a seed if you want reproducible results
-#' tsne_out <- Rtsne(iris_matrix) # Run TSNE
+#' 
+#' # Set a seed if you want reproducible results
+#' set.seed(42)
+#' tsne_out <- Rtsne(iris_matrix,pca=FALSE,perplexity=30,theta=0.0) # Run TSNE
 #' 
 #' # Show the objects in the 2D tsne representation
-#' plot(tsne_out$Y,col=iris_unique$Species)
+#' plot(tsne_out$Y,col=iris_unique$Species, asp=1)
+#' 
+#' # data.frame as input
+#' tsne_out <- Rtsne(iris_unique,pca=FALSE, theta=0.0)
 #' 
 #' # Using a dist object
-#' tsne_out <- Rtsne(dist(iris_matrix))
-#' plot(tsne_out$Y,col=iris_unique$Species)
+#' set.seed(42)
+#' tsne_out <- Rtsne(dist(normalize_input(iris_matrix)), theta=0.0)
+#' plot(tsne_out$Y,col=iris_unique$Species, asp=1)
 #' 
-#' # Use a given initialization of the locations of the points
-#' tsne_part1 <- Rtsne(iris_unique[,1:4], theta=0.0, pca=FALSE,max_iter=350)
-#' tsne_part2 <- Rtsne(iris_unique[,1:4], theta=0.0, pca=FALSE, max_iter=150,Y_init=tsne_part1$Y)
+#' set.seed(42)
+#' tsne_out <- Rtsne(as.matrix(dist(normalize_input(iris_matrix))),theta=0.0)
+#' plot(tsne_out$Y,col=iris_unique$Species, asp=1)
+#' 
+#' # Supplying starting positions (example: continue from earlier embedding)
+#' set.seed(42)
+#' tsne_part1 <- Rtsne(iris_unique[,1:4], theta=0.0, pca=FALSE, max_iter=350)
+#' tsne_part2 <- Rtsne(iris_unique[,1:4], theta=0.0, pca=FALSE, max_iter=650, Y_init=tsne_part1$Y)
+#' plot(tsne_part2$Y,col=iris_unique$Species, asp=1)
+#' \dontrun{
+#' # Fast PCA and multicore
+#' 
+#' tsne_out <- Rtsne(iris_matrix, theta=0.1, partial_pca = TRUE, initial_dims=3)
+#' tsne_out <- Rtsne(iris_matrix, theta=0.1, num_threads = 2)
+#' }
 #' @useDynLib Rtsne, .registration = TRUE
 #' @import Rcpp
 #' @importFrom stats model.matrix na.fail prcomp
